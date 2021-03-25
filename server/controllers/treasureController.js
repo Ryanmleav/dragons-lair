147859c6e17c883c28c159666878f54aed9daef0
Ryan Leavitt <hopelessgains@gmail.com>
2021-03-25 00:04:11 -0600

    finshed lab

--- server/controllers/treasureController.js
+++ server/controllers/treasureController.js
@@ -1,3 +1,5 @@
+const { usersOnly } = require("../middleware/authMiddleware")
+
 module.exports = {
   dragonTreasure: async (req, res) => {
     const treasure = await req.app.get('db').get_dragon_treasure(1)
@@ -12,5 +14,9 @@ module.exports = {
     const { id } = req.session.user;
     const userTreasure = await req.app.get('db').add_user_treasure([ treasureURL, id ])
     return res.status(200).send(userTreasure)
+  },
+  getAllTreasure: async (req, res) => {
+    const allTreasure = await req.app.get('db').get_all_treasure()
+    return res.status(200).send(allTreasure)
   }
 }
--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ app.get('/auth/logout', authCtrl.logout)
 app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
 app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure)
 app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure)
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure)
 
 massive({
   connectionString: CONNECTION_STRING,
